44ddef3d4220db497c8f79f3e8642955ee8bde06
ƬHE ЯAW  ☣ <jasht.sserie@gmail.com>
2017-08-11 21:33:31 -0400

    Update and rename ban-em.sh to ips.sh

--- extra/ban-em.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo 1 > /proc/sys/net/ipv4/ip_forward
-route add -net 149.202.206.0 netmask 255.255.255.0 reject
-route add -net 123.103.255.0 netmask 255.255.255.0 reject
-route add -net 62.210.244.0 netmask 255.255.255.0 reject
-route add -net 185.73.0.0 netmask 255.255.0.0 reject
---
+++ extra/ips.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+echo 1 > /proc/sys/net/ipv4/ip_forward
+iptables -F
+iptables -A INPUT -m conntrack --ctstate ESTABLISHED,RELATED -j ACCEPT
+iptables -A INPUT -p tcp --dport 22 -j ACCEPT
+
+# Your Stream Port
+iptables -A INPUT -p tcp --dport 8080 -j ACCEPT
+#
+
+iptables -I INPUT 1 -i lo -j ACCEPT
+iptables -A INPUT -j DROP
+
+iptables -t nat -I OUTPUT --dest 149.202.206.51/28 -j DNAT --to-destination 127.0.0.1
+iptables -t nat -I OUTPUT --dest 123.103.255.80/28 -j DNAT --to-destination 127.0.0.1
+iptables -t nat -I OUTPUT --dest 62.210.244.112/28 -j DNAT --to-destination 127.0.0.1
+iptables -t nat -I OUTPUT --dest 185.73.239.0/28 -j DNAT --to-destination 127.0.0.1
+apt-get update
+apt-get install iptables-persistent
+sudo invoke-rc.d iptables-persistent save
+
+/sbin/iptables -t nat -I OUTPUT --dest 149.202.206.51/28 -j DNAT --to-destination 127.0.0.1
+/sbin/iptables -t nat -I OUTPUT --dest 123.103.255.80/28 -j DNAT --to-destination 127.0.0.1
+/sbin/iptables -t nat -I OUTPUT --dest 62.210.244.112/28 -j DNAT --to-destination 127.0.0.1
+/sbin/iptables -t nat -I OUTPUT --dest 185.73.239.0/28 -j DNAT --to-destination 127.0.0.1
+sudo invoke-rc.d iptables-persistent save
